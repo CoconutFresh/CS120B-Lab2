c4c3d6468066a8648b98493ed274e7648fd01873
CoconutFresh <GlennBersabe@gmail.com>
2019-01-13 17:32:19 -0800

    Added appropiate header

--- Gbers002_Bzhan014_lab2_part1/Gbers002_Bzhan014_lab2_part1/main.c
+++ Gbers002_Bzhan014_lab2_part1/Gbers002_Bzhan014_lab2_part1/main.c
@@ -1,28 +1,34 @@
+/*	Glenn Bersabe Email: Gbers002@ucr.edu
+	Bohan Zhang Email: Bzhan014@ucr.edu
+ *	Lab Section: 023
+ *	Assignment: Lab 2  Exercise 1 
+ *	Exercise Description: Checks to see if pin A0 and A1 are of certain values. Sets B0 to respective value.
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
 #include <avr/io.h>
 
 int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-	unsigned char tmpA0 = 0x01; // Temporary variable to hold the value of B
-	unsigned char tmpA1 = 0x00; // Temporary variable to hold the value of A
+	unsigned char tmpA0 = 0x01; // Temporary variable to hold the value of Pin A0
+	unsigned char tmpA1 = 0x00; // Temporary variable to hold the value of A1
 	unsigned char tmpB = 0x00;
 	while(1)
 	{
 		tmpA0 = PINA & 0x01;
 		tmpA1 = PINA & 0x00;
-		//tmpA0 = PINA & 0x01;
-		//tmpA1 = PINA & 0x00;
-		//tmpB = PORTB & 0x00;
 		
 		if (tmpA0 == 0x01 && tmpA1 ==0x00) {
-			//tmpB = tmpB & 0x01;
-			tmpB = 0x01;
+			tmpB = 0x01; //Case that sets Port B0 to 1
 		}
 		else {
-			tmpB = 0x00;
+			tmpB = 0x00; //All other cases set B0 to 0
 		}
-		PORTB = tmpB;
+		PORTB = tmpB; 
 	}
 	return 0;
 }
---
+++ gbers002_bzhan014_lab2_part2/gbers002_bzhan014_lab2_part2/main.c
@@ -0,0 +1,18 @@
+/*
+ * gbers002_bzhan014_lab2_part2.c
+ *
+ * Created: 1/13/2019 5:30:28 PM
+ * Author : Glenn
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
